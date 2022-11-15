5cbacd7b0cb1584e2f35d5d0fe1c4c8bf33e71fc
Austin Webre <austin@thewebres.com>
2022-11-14 22:33:06 -0600

    test css on custom domain

--- .eleventy.js
+++ .eleventy.js
@@ -16,10 +16,16 @@ module.exports = function (eleventyConfig) {
   // Watch targets
   eleventyConfig.addWatchTarget("./src/styles/");
 
+  var pathPrefix = "";
+  if (process.env.GITHUB_REPOSITORY) {
+    pathPrefix = process.env.GITHUB_REPOSITORY.split("/")[1];
+  }
+
   return {
     dir: {
       input: "src",
     },
+    pathPrefix,
   };
 };
 
