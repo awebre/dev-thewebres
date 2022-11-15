99e95a38497dcc2219bee80882ff6874aadbc1a9
Austin Webre <austin@thewebres.com>
2022-11-14 22:23:44 -0600

    fix issue with production prefix

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,13 @@
 const fs = require("fs");
 const htmlmin = require("html-minifier");
 
-module.exports = function(eleventyConfig) {
-
+module.exports = function (eleventyConfig) {
   if (process.env.ELEVENTY_PRODUCTION) {
     eleventyConfig.addTransform("htmlmin", htmlminTransform);
   } else {
-    eleventyConfig.setBrowserSyncConfig({ callbacks: { ready: browserSyncReady }});
+    eleventyConfig.setBrowserSyncConfig({
+      callbacks: { ready: browserSyncReady },
+    });
   }
 
   // Passthrough
@@ -15,22 +16,16 @@ module.exports = function(eleventyConfig) {
   // Watch targets
   eleventyConfig.addWatchTarget("./src/styles/");
 
-  var pathPrefix = "";
-  if (process.env.GITHUB_REPOSITORY) {
-    pathPrefix = process.env.GITHUB_REPOSITORY.split('/')[1];
-  }
-
   return {
     dir: {
-      input: "src"
+      input: "src",
     },
-    pathPrefix
-  }
+  };
 };
 
 function browserSyncReady(err, bs) {
   bs.addMiddleware("*", (req, res) => {
-    const content_404 = fs.readFileSync('_site/404.html');
+    const content_404 = fs.readFileSync("_site/404.html");
     // Add 404 http status code in request header.
     res.writeHead(404, { "Content-Type": "text/html; charset=ETF-8" });
     // Provides the 404 content without redirect.
@@ -40,11 +35,11 @@ function browserSyncReady(err, bs) {
 }
 
 function htmlminTransform(content, outputPath) {
-  if( outputPath.endsWith(".html") ) {
+  if (outputPath.endsWith(".html")) {
     let minified = htmlmin.minify(content, {
       useShortDoctype: true,
       removeComments: true,
-      collapseWhitespace: true
+      collapseWhitespace: true,
     });
     return minified;
   }
